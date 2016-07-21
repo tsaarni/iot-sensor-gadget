d6b53032adad1f3878811338894338f8cf683fab
Tero Saarni <tero.saarni@gmail.com>
2016-07-21 19:36:00 +0300

    added battery sensor

--- sensor-node/src/main.cpp
+++ sensor-node/src/main.cpp
@@ -7,28 +7,31 @@
 #include "logging.hpp"
 #include "gateway.hpp"
 #include "nodeid.hpp"
+
 #include "htu32dsensor.hpp"
 #include "doorsensor.hpp"
 #include "lightsensor.hpp"
+#include "batterysensor.hpp"
 
 
 const char topic_prefix[] PROGMEM = "myhome";
-
-
 const int random_seed_pin PROGMEM = A3;
 
 Gateway gateway(/*ce*/  9, /*cs*/ 10, topic_prefix);
+NodeId node_id;
+
+// sensors
 DoorSensor door(/*pin*/ 8);
 LightSensor light(/*pin*/ 14);
 Htu32dSensor temp_humid;
-NodeId node_id;
-
+BatterySensor battery;
 
 void
 read_sensors()
 {
    temp_humid.publish();
    light.publish();
+   battery.publish();
 }
 
 
@@ -36,9 +39,12 @@ void setup()
 {
    Serial.begin(57600);
 
+   // seed pseudo-random algorithm with unconnected analog pin 
    pinMode(random_seed_pin, INPUT);
-   delay(500);
+   delay(10);
    randomSeed(analogRead(random_seed_pin));
+
+   // restore pull-up to limit current leakage
    pinMode(random_seed_pin, INPUT_PULLUP);
    
    gateway.register_node(node_id.get());
