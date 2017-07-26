bdb6772a8b73b9d072f03421d040a1c6b8ed0cb8
Tero Saarni <tero.saarni@gmail.com>
2017-07-26 12:13:20 +0300

    python gateway

--- gateways/rpi-python/gateway.py
+++ gateways/rpi-python/gateway.py
@@ -1,12 +1,41 @@
 #!/usr/bin/env python
 
-import nrf24
+import time
+import RF24
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
 
 
-radio = nrf24.NRF24()
-radio.begin(1, 0, "P9_23", "P9_24")
-radio.openReadingPipe(1, ['1','m','q','t','t'])
+radio = RF24.RF24(RPI_BPLUS_GPIO_J8_22, RPI_BPLUS_GPIO_J8_24, BCM2835_SPI_SPEED_8MHZ)
+mqttc = mqtt.Client()
 
 
-buf = []
-radio.read(buf)
+def radio_interrupt(channel):
+    if radio.available():
+        while radio.available():
+            len = radio.getDynamicPayloadSize()
+            buf = radio.read(len)
+            print('Got payload size={} value="{}"'.format(len, receive_payload.decode('utf-8')))
+
+
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(2, GPIO.FALLING, callback=radio_interrupt)
+
+
+radio.begin()
+radio.enableDynamicPayloads()
+radio.setRetries(5,15)
+radio.printDetails()
+
+radio.openWritingPipe(pipes[1])
+radio.openReadingPipe(1, pipe_name)
+radio.startListening()
+
+connect(host, port=1883, keepalive=60, bind_address="")
+
+while 1:
+    time.sleep(1000)
